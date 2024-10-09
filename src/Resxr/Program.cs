00e05cec4c5c9f5e26bb2738eb9b161aca6d635a
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2024-10-09 15:00:01 +0200

    Change file to be argument instead of option

--- src/Resxr/Program.cs
+++ src/Resxr/Program.cs
@@ -1,14 +1,8 @@
-using System;
-using System.IO;
-using System.Text;
-using System.Linq;
-using System.Xml.Linq;
 using System.CommandLine;
-using Resxr.Commands;
-using System.CommandLine.Invocation;
-using Microsoft.Extensions.Hosting;
+using System.IO;
 using Microsoft.Extensions.DependencyInjection;
-using Resxr.Commands.Translation;
+using Microsoft.Extensions.Hosting;
+using Resxr.Commands;
 
 namespace Resxr
 {
@@ -17,36 +11,47 @@ namespace Resxr
         public static int Main(string[] args)
         {
             var builder = new HostBuilder()
-               .ConfigureServices((hostContext, services) =>
-               {
-                   services.AddScoped<GeneratorCommand>();
-                   services.AddScoped<TranslateMissingKeysCommand>();
-                   services.AddHttpClient();
-               }).UseConsoleLifetime();
+                .ConfigureServices(
+                    (hostContext, services) =>
+                    {
+                        services.AddScoped<GeneratorCommand>();
+                        services.AddScoped<TranslateMissingKeysCommand>();
+                        services.AddHttpClient();
+                    }
+                )
+                .UseConsoleLifetime();
 
             var host = builder.Build();
 
-            var fileOption = new Option<FileInfo>(
-                name: "--file",
+            var fileArgument = new Argument<FileInfo>(
+                name: "file",
                 description: "The root .resx file to work with"
             );
 
-            var rootCommand = new RootCommand("Resxr, work with .resx files from the comfort of your terminal.");
+            var rootCommand = new RootCommand(
+                "Resxr, work with .resx files from the comfort of your terminal."
+            );
 
             // 1. Generate designer.cs file
-            var generateCommand = new Command("generate", "Generate a .Designer.cs file based an input resx")
+            var generateCommand = new Command(
+                "generate",
+                "Generate a .Designer.cs file based an input resx"
+            )
             {
-                fileOption,
+                fileArgument,
             };
 
-            generateCommand.SetHandler(async (file) =>
-            {
-                var command = host.Services.GetService<GeneratorCommand>();
-                await command.Invoke(file);
-            }, fileOption);
+            generateCommand.SetHandler(
+                async (file) =>
+                {
+                    var command = host.Services.GetService<GeneratorCommand>();
+                    await command.Invoke(file);
+                },
+                fileArgument
+            );
             rootCommand.Add(generateCommand);
 
-            // 2. Add missing keys to 
+            // 2. Add missing keys to
             var cultureOption = new Option<string>(
                 name: "--culture",
                 description: "The culture to translate"
@@ -55,17 +60,25 @@ namespace Resxr
                 name: "--provider",
                 description: "The translation provider to use"
             );
-            var translateCommand = new Command("translate", "Updates language .resx file to include missing keys and to translate them")
+            var translateCommand = new Command(
+                "translate",
+                "Updates language .resx file to include missing keys and to translate them"
+            )
             {
-                fileOption,
                 cultureOption,
                 providerOption,
+                fileArgument,
             };
-            translateCommand.SetHandler(async (file, culture, provider) =>
-            {
-                var command = host.Services.GetService<TranslateMissingKeysCommand>();
-                await command.InvokeAsync(file, culture, provider);
-            }, fileOption, cultureOption, providerOption);
+            translateCommand.SetHandler(
+                async (file, culture, provider) =>
+                {
+                    var command = host.Services.GetService<TranslateMissingKeysCommand>();
+                    await command.InvokeAsync(file, culture, provider);
+                },
+                fileArgument,
+                cultureOption,
+                providerOption
+            );
             rootCommand.Add(translateCommand);
 
             return rootCommand.InvokeAsync(args).Result;
